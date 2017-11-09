ce4a09937cf2d04e5f50451d0a2d1ac1e3c68d13
Yunus Emre Uluçay <ye.ulucay@gmail.com>
2017-11-09 17:32:00 +0300

    template added

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'taskbuster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,6 +74,8 @@ TEMPLATES = [
         },
     },
 ]
+
+print(os.path.join(BASE_DIR, "templates"))
 
 WSGI_APPLICATION = 'taskbuster.wsgi.application'
 
@@ -126,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
