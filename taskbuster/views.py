8ee0c8f3d7c4b84512f8f60c37c59f51bdcc3e65
Yunus Emre Ulucay <ye.ulucay@gmail.com>
2017-11-09 20:11:58 +0300

    template organized

--- taskbuster/views.py
+++ taskbuster/views.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
- 
+
+
 def home(request):
     return render(request, "taskbuster/index.html", {})
+
+def home_files(request, filename):
+    return render(request, filename, {}, content_type="text/plain")
